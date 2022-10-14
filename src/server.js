84b51b99c012321bb42c3f33a9620651e483fcac
Your Name <you@example.com>
2022-10-13 20:44:00 -0400

    add upvote article

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 import express from 'express';
 
-const app = express();
+// PUT /articles/learn-react/upvote
 
-app.get('/hello', (req,res)=>{
-    res.send("PICKCHU");
+let articlesInfo = [{
+    name:'learn-react',
+    upvotes:0,
+
+}, {
+    name:'learn-node',
+    upvotes:0,
+}, {
+    name:'mongodb',
+    upvotes:0,
+}]
+
+
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote',(req,res)=>{
+    const {name } =req.params;
+    const article = articlesInfo.find(a => a.name ===name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
 });
 
 app.listen(8888,()=>{
