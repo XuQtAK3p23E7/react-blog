49f73070f49f68ea98243dca5753c09ab307bcaf
M <m@Ms-Mac-mini.local>
2022-10-23 15:50:37 -0400

    simply repeat code to db.js with callback

--- src/server.js
+++ src/server.js
@@ -1,17 +1,11 @@
 import express from 'express';
-import {MongoClient} from 'mongodb';
+import {db, connectToDb} from './db.js';
 
 const app = express();
 app.use(express.json());
 
 app.get('/api/articles/:name', async (req,res) =>{
     const {name} =req.params;
-
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
-
 
     const article = await db.collection('articles').findOne({name});
 
@@ -28,10 +22,6 @@ app.get('/api/articles/:name', async (req,res) =>{
 app.put('/api/articles/:name/upvote',async (req,res)=>{
     const {name } =req.params;
     
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({name},{
         $inc:{ upvotes: 1},
     });
@@ -47,11 +37,7 @@ app.put('/api/articles/:name/upvote',async (req,res)=>{
 app.post('/api/articles/:name/comments',async (req,res)=>{
     const {name} = req.params;
     const {postedBy, text } = req.body;
-    // connects to Mongo driver
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-    // connects to Mongo database
-    const db = client.db('react-blog-db');
+    
     await db.collection('articles').updateOne({name}, { 
         $push: {comments:{postedBy,text}},
 
@@ -66,9 +52,13 @@ app.post('/api/articles/:name/comments',async (req,res)=>{
 
 })
 
-app.listen(8888,()=>{
-    console.log("SERVER IS LISTENING ON PORT 8888");
+connectToDb(()=>{
+    console.log("SUCCESS CONNECT TO DATABASE");
+    app.listen(8888,()=>{
+        console.log("SERVER IS LISTENING ON PORT 8888");
+    })
 })
+
 
 
 
