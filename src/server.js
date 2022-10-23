8298b4836533d37649bafa7033e0e51395bcd2c2
M <m@Ms-Mac-mini.local>
2022-10-23 13:41:10 -0400

    new api point

--- src/server.js
+++ src/server.js
@@ -1,25 +1,18 @@
 import express from 'express';
-
-// PUT /articles/learn-react/upvote
-
-let articlesInfo = [{
-    name:'learn-react',
-    upvotes:0,
-    comments: [],
-
-}, {
-    name:'learn-node',
-    upvotes:0,
-    comments: [],
-}, {
-    name:'mongodb',
-    upvotes:0,
-    comments: [],
-}]
-
+import {MongoClient} from 'mongodb';
 
 const app = express();
 app.use(express.json());
+
+app.get('/api/articles/:name', async (req,res) =>{
+    const {name} =req.params;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+    
+});
 
 app.put('/api/articles/:name/upvote',(req,res)=>{
     const {name } =req.params;
